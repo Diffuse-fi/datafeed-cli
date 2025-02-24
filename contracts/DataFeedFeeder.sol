a2cba4a15268830cef9fd08284ae0f1da3ced729
Aleksandr Cherenkov <aleksandr.cherenkov.3@gmail.com>
2025-02-24 11:34:49 +0000

    check mrenclave in set_zk

--- contracts/DataFeedFeeder.sol
+++ contracts/DataFeedFeeder.sol
@@ -27,6 +27,9 @@ contract DataFeedFeeder {
 
     mapping (string => DataFeedStorage) dataFeedStorages;
     uint constant PAIRS_AMOUNT = 7; // hardcoded pairs
+    uint16 constant ENCLAVE_REPORT_OFFSET_OUTPUT = 13;
+    uint16 constant MRENCLAVE_OFFSET = 64;
+    uint16 constant REPORT_DATA_OFFSET = 320;
 
     constructor(
         IAutomataDcapAttestationFee _sgx_quote_verifier,
@@ -39,6 +42,18 @@ contract DataFeedFeeder {
         }
     }
 
+    // ecnalveReport starts at ENCLAVE_REPORT_OFFSET_OUTPUT-th byte of the verification output
+    function check_mrenclave(bytes memory verificationOutput) private pure {
+        bytes memory mrEnclaveReal = new bytes(32);
+        for (uint i = 0; i < 32; i++) {
+            // mrenclave starts at byte 64 of enclaveReport and is 32 bytes long
+            // https://github.com/automata-network/automata-dcap-attestation/blob/3a854a31eb2345a31f9e33697eef0d814d031a12/evm/contracts/bases/QuoteVerifierBase.sol#L64-L76
+            mrEnclaveReal[i] = verificationOutput[ENCLAVE_REPORT_OFFSET_OUTPUT + 64 + i];
+        }
+        bytes32 mrEnclaveExpected = 0xc1aeaf6d637c85bb0d28bb580054cf5336d79e19f5eff8fa50389715ed112362;
+        require (bytes32(mrEnclaveReal) == mrEnclaveExpected, "mrEnclave from input differs from expected!");
+    }
+
     function set_zk(
         string[PAIRS_AMOUNT] calldata pair_names,
         uint256[PAIRS_AMOUNT] calldata prices,
@@ -46,42 +61,15 @@ contract DataFeedFeeder {
         bytes calldata sgx_verification_journal,
         bytes calldata sgx_verification_seal
     ) external payable {
-        // payable: sgx_quote_verifier collects fee, but it is 0 on sepolia, so now it will work with msg.value = 0
 
-        // verify sgx_quote
         (bool success, bytes memory output) = sgx_quote_verifier.verifyAndAttestWithZKProof{value: msg.value}(sgx_verification_journal, 1, sgx_verification_seal);
         if (!success) {
             // fail returns bytes(error_string)
             // success returns custom output type:
             // https://github.com/automata-network/automata-dcap-attestation/blob/b49a9f296a5e0cd8b1f076ec541b1239199cadd2/contracts/verifiers/V3QuoteVerifier.sol#L154
-            require(success, string(output)); // found one or more collaterals mismatch
+            require(success, string(output));
         }
-
-        bytes32[] memory hashes = new bytes32[](PAIRS_AMOUNT * 3);
-
-        for (uint256 i = 0; i < PAIRS_AMOUNT; i++) {
-            hashes[i*3] = keccak256(abi.encodePacked(pair_names[i]));
-            hashes[i*3 + 1] = keccak256(abi.encodePacked(prices[i]));
-            hashes[i*3 + 2] = keccak256(abi.encodePacked(timestamps[i]));
-        }
-        bytes memory concatenated;
-
-        for (uint256 i = 0; i < hashes.length; i++) {
-            concatenated = abi.encodePacked(concatenated, hashes[i]);
-        }
-
-        bytes32 hashed_input_data = keccak256(concatenated);
-
-        // extract hashed data from quote
-        require(sgx_verification_journal.length >= 335 + 32, "SGX quote too short");
-        bytes memory data_hash_from_sgx = new bytes(32);
-        for (uint i = 0; i < 32; i++) {
-            // TODO: maybe extract from verification output, not input?
-            data_hash_from_sgx[i] = sgx_verification_journal[335 + i];
-        }
-
-        require(hashed_input_data ==bytes32(data_hash_from_sgx), "hashed_input_data != data_hash_from_sgx");
-        set(pair_names, prices, timestamps);
+        set(output, pair_names, prices, timestamps);
     }
 
     function set_onchain(
@@ -90,9 +78,7 @@ contract DataFeedFeeder {
         uint256[PAIRS_AMOUNT] calldata timestamps,
         bytes calldata sgx_quote
     ) external payable {
-        // payable: sgx_quote_verifier collects fee, but it is 0 on sepolia, so now it will work with msg.value = 0
 
-        // verify sgx_quote
         (bool success, bytes memory output) = sgx_quote_verifier.verifyAndAttestOnChain{value: msg.value}(sgx_quote);
         if (!success) {
             // fail returns bytes(error_string)
@@ -100,22 +86,17 @@ contract DataFeedFeeder {
             // https://github.com/automata-network/automata-dcap-attestation/blob/b49a9f296a5e0cd8b1f076ec541b1239199cadd2/contracts/verifiers/V3QuoteVerifier.sol#L154
             require(success, string(output));
         }
+        set(output, pair_names, prices, timestamps);
+    }
 
-        // extract hashed input from quote
-        // enclave report starts after header so we skip first HEADER_LENGTH bytes, next we extract
-        // https://github.com/automata-network/automata-dcap-attestation/blob/3a854a31eb2345a31f9e33697eef0d814d031a12/evm/contracts/bases/QuoteVerifierBase.sol#L64-L76
-        bytes memory mrEnclaveReal = new bytes(32);
-        for (uint i = 0; i < 32; i++) {
-            mrEnclaveReal[i] = sgx_quote[HEADER_LENGTH + 64 + i];
-        }
-        bytes32 mrEnclaveExpected = 0xc1aeaf6d637c85bb0d28bb580054cf5336d79e19f5eff8fa50389715ed112362;
-        require (bytes32(mrEnclaveReal) == mrEnclaveExpected, "mrEnclave from input sgx_quote differs from expected!");
+    function set(
+        bytes memory output,
+        string[PAIRS_AMOUNT] calldata pair_names,
+        uint256[PAIRS_AMOUNT] calldata prices,
+        uint256[PAIRS_AMOUNT] calldata timestamps
+    ) internal {
 
-
-        bytes memory data_hash_from_sgx = new bytes(32);
-        for (uint i = 0; i < 32; i++) {
-            data_hash_from_sgx[i] = sgx_quote[HEADER_LENGTH + 320 + i];
-        }
+        check_mrenclave(output);
 
         bytes32[] memory hashes = new bytes32[](PAIRS_AMOUNT * 3);
 
@@ -132,15 +113,13 @@ contract DataFeedFeeder {
 
         bytes32 hashed_input_data = keccak256(concatenated);
 
-        require(hashed_input_data == bytes32(data_hash_from_sgx), "hashed_input_data != data_hash_from_sgx");
-        set(pair_names, prices, timestamps);
-    }
 
-    function set(
-        string[PAIRS_AMOUNT] calldata pair_names,
-        uint256[PAIRS_AMOUNT] calldata prices,
-        uint256[PAIRS_AMOUNT] calldata timestamps
-    ) internal {
+        bytes memory data_hash_from_sgx = new bytes(32);
+        for (uint i = 0; i < 32; i++) {
+            data_hash_from_sgx[i] = output[ENCLAVE_REPORT_OFFSET_OUTPUT + REPORT_DATA_OFFSET + i];
+        }
+        require(hashed_input_data == bytes32(data_hash_from_sgx), "hashed_input_data != data_hash_from_sgx");
+
         // send round data to storage contracts
         for (uint i = 0; i < pair_names.length; i++) {
             dataFeedStorages[pair_names[i]].setNewRound(prices[i], timestamps[i]);
