dba97b845d3c789ca10f5a9b5492377016f66be8
akablockchain1 <akablockchain1@gmail.com>
2025-03-17 15:26:47 +0300

    Template for scheduled parse and prove feed service is implemeted

---
+++ services/scheduled_parse_prove_feed/parse_and_prove_feed.sh
@@ -0,0 +1,5 @@
+echo "start feeding: $(date)" >> /var/logs/diffuse/parse_and_prove_feed.log
+
+echo "execution" >> parse_and_prove_feed.log 2>&1
+
+echo "binance data request and proving finished: $(date)" >> /var/logs/diffuse/parse_and_prove_feed.log
