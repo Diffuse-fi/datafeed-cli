cc723dae7bb1eaa2cbd312eb51dc38fd890839f7
Aleksandr Cherenkov <aleksandr.cherenkov.3@gmail.com>
2025-03-28 13:57:19 +0000

    rewrite verify_contract using hardcoded ata urls

--- cli/verify_contract.py
+++ cli/verify_contract.py
@@ -8,36 +8,16 @@ def verify_storage(net, pair_name):
     pair_address =  call_contract(net, get_feeder_address(net), "getPairStorageAddress(string)(address)", [pair_name], is_address=True)
 
     cmd = [
-        "cast",
-        "abi-encode",
-        "f(string memory _description_string, uint8 _decimals_amount)",
-        pair_name,
-        "8"
-    ]
-
-    res = subprocess.run(cmd, capture_output=True, text=True)
-
-    if res.returncode != 0:
-        print("res.stdout:", res.stdout)
-        print("res.stderr:", res.stderr)
-        sys.exit(1)
-    abi_encoded_constructor_args = res.stdout.strip()
-    print("abi_encoded_constructor_args:", abi_encoded_constructor_args)
-    cmd = [
         'forge',
         'verify-contract',
         '--rpc-url',
-        'https://testnet-rpc.monad.xyz',
+        'https://automata-mainnet.alt.technology',
         '--verifier',
-        'sourcify',
+        'blockscout',
         '--verifier-url',
-        'https://sourcify-api-monad.blockvision.org',
+        'https://automata-mainnet-explorer.alt.technology:443/api/',
         pair_address,
         'contracts/DataFeedStorage.sol:DataFeedStorage',
-
-        # '--etherscan-api-key=' + os.getenv('ETHERSCAN_API_KEY'),
-        # '--watch',
-        # '--constructor-args=' + abi_encoded_constructor_args
     ]
 
     res = subprocess.run(cmd, capture_output=True, text=True)
@@ -54,13 +34,35 @@ def verify_feeder(net):
         'forge',
         'verify-contract',
         '--rpc-url',
-        'https://testnet-rpc.monad.xyz',
+        'https://automata-mainnet.alt.technology',
         '--verifier',
-        'sourcify',
+        'blockscout',
         '--verifier-url',
-        'https://sourcify-api-monad.blockvision.org',
+        'https://automata-mainnet-explorer.alt.technology:443/api/',
         get_feeder_address(net),
         'contracts/DataFeedFeeder.sol:DataFeedFeeder',
+    ]
+
+    res = subprocess.run(cmd, capture_output=True, text=True)
+    print("res.stdout:", res.stdout)
+    print("res.stderr:", res.stderr)
+
+    if res.returncode != 0:
+        print("res.returncode:", res.returncode)
+        sys.exit(1)
+
+def verify_proxy(net):
+    cmd = [
+        'forge',
+        'verify-contract',
+        '--rpc-url',
+        'https://automata-mainnet.alt.technology',
+        '--verifier',
+        'blockscout',
+        '--verifier-url',
+        'https://automata-mainnet-explorer.alt.technology:443/api/',
+        get_proxy_address(net),
+        'contracts/DataFeedProxy.sol:DataFeedProxy',
     ]
 
     res = subprocess.run(cmd, capture_output=True, text=True)
@@ -76,7 +78,8 @@ def main():
     parser = argparse.ArgumentParser(description="Data feeder parameters")
     parser.add_argument('-n', '--network', type=network_class, required=True, help="Choose network (" + networks_str + ")")
     args = parser.parse_args()
-    # verify_feeder(args.network)
+    verify_proxy(args.network)
+    verify_feeder(args.network)
     for pair_name in pair_names_list:
         verify_storage(args.network, pair_name)
 
@@ -85,10 +88,3 @@ def main():
 if __name__ == "__main__":
     main()
 
-
-#     forge verify-contract \
-#   --rpc-url https://testnet-rpc.monad.xyz \
-#   --verifier sourcify \
-#   --verifier-url 'https://sourcify-api-monad.blockvision.org' \
-#   0xF27E6e2Ad3e3c24c44adC271fC8576357474BAE0 \
-#   contracts/DataFeedProxy.sol:DataFeedProxy
